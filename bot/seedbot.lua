7262e0cf17c62a49ab007aa85df30531f60f4447
jeiboor <antispam0099@Gmail.com>
2016-02-05 15:40:31 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -202,6 +202,25 @@ function load_config( )
   end
   return config
 end
+local function run(msg, matches)
+  local text = matches[1]
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  return text
+end
+
+return {
+  description = "Simplest plugin ever!",
+  usage = "!echo [whatever]: echoes the msg",
+  patterns = {
+    "^!echo(.+)$"
+  }, 
+  run = run 
+}
 
 -- Create a basic config.json file and saves it.
 function create_config( )
@@ -382,6 +401,9 @@ Set <text> as about
 
 !settings
 Returns group settings
+
+!echo <text> 
+Echo <text> 
 
 !newlink
 create/revoke your group link
